1c9c6f8cd1c07fac840e86becf77ab794f8a854f
Magicalex <magicalex@mondedie.fr>
2019-12-10 15:46:17 +0100

    test(): authentication dockerhub

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,5 +8,5 @@ echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdi
 # docker build --tag mondedie/rutorrent:filebot --build-arg FILEBOT=YES .
 
 # push to dockerhub
-docker push mondedie/rutorrent:latest
-docker push mondedie/rutorrent:filebot
+# docker push mondedie/rutorrent:latest
+# docker push mondedie/rutorrent:filebot
