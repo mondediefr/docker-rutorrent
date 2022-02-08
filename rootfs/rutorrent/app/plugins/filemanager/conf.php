21f297b8f3f183a8783b41273c2d694e575dad56
Micdu70 <Micdu70@gmx.fr>
2022-02-08 08:04:50 +0100

    Update filemanager plugin conf file

--- rootfs/rutorrent/app/plugins/filemanager/conf.php
+++ rootfs/rutorrent/app/plugins/filemanager/conf.php
@@ -11,25 +11,33 @@ $config['show_fullpaths'] = false; // wheter to show userpaths or full system pa
 $config['textExtensions'] = 'log|txt|nfo|sfv|xml|html';
 
 // see what 7zip extraction supports as type by file extension
-$config['fileExtractExtensions'] = '7z|bzip2|t?bz2|t?g|gz[ip]?|iso|img|lzma|rar|tar|t?xz|zip|z01|wim';
+$config['fileExtractExtensions'] = '7z|bzip2|t?bz2|tgz|gz(ip)?|iso|img|lzma|rar|tar|t?xz|zip|z01|wim';
 
 // archive creation, see archiver man page before editing
 // archive.fileExt -> config
 $config['archive']['type'] = [
     // 'rar' => [
-    //     'bin' =>'rar',
-    //     'compression' => range(0, 5),
+    //     'bin' => 'rar',
+    //     'compression' => [0, 3, 5],
     // ],
     '7z' => [
-        'bin' =>'7zip',
-        'compression' => [0, 5, 9],
+        'bin' => '7zip',
+        'compression' => [1, 5, 9],
     ]];
 
 $config['archive']['type']['zip'] = $config['archive']['type']['7z'];
 $config['archive']['type']['tar'] = $config['archive']['type']['7z'];
-$config['archive']['type']['gzip'] = $config['archive']['type']['7z'];
-$config['archive']['type']['tgz'] = $config['archive']['type']['7z'];
-$config['archive']['type']['tar.bz2'] = $config['archive']['type']['7z'];
-$config['archive']['type']['bzip2'] = $config['archive']['type']['7z'];
+$config['archive']['type']['tar']['has_password'] = false;
+$config['archive']['type']['bz2'] = $config['archive']['type']['tar'];
+$config['archive']['type']['gz'] = $config['archive']['type']['tar'];
+$config['archive']['type']['tar.7z'] = $config['archive']['type']['tar'];
+$config['archive']['type']['tar.bz2'] = $config['archive']['type']['tar'];
+$config['archive']['type']['tar.gz'] = $config['archive']['type']['tar'];
+$config['archive']['type']['tar.xz'] = $config['archive']['type']['tar'];
 
 
+// multiple passes for archiving and compression
+$config['archive']['type']['tar.gz']['multipass'] = ['tar', 'gzip'];
+$config['archive']['type']['tar.bz2']['multipass'] = ['tar', 'bzip2'];
+$config['archive']['type']['tar.7z']['multipass'] = ['tar', '7z'];
+$config['archive']['type']['tar.xz']['multipass'] = ['tar', 'xz'];
